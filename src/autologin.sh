6fb4efbbbc99b8785e5ce430f9b14386e1e16829
MR-Addict <2750417853@qq.com>
2022-09-21 16:04:34 +0800

    update connection check method

--- src/autologin.sh
+++ src/autologin.sh
@@ -14,15 +14,15 @@ else
   echo "[FAIL] $(date)" >> log.txt && exit
 fi
 
-# Store data in login_get_html.txt and login_cookie.txt
+# Store data in login_get_html.html and login_cookie.txt
 echo "[INFO] Fetching data from remote host..."
-curl -skL i.njtech.edu.cn -c login_cookie.txt -o login_get_html.txt
+curl -skL i.njtech.edu.cn -c login_cookie.txt -o login_get_html.html
 
 posturl="https://u.njtech.edu.cn/cas/login?service=https%3A%2F%2Fu.njtech.edu.cn%2Foauth2%2Fauthorize%3Fclient_id%3DOe7wtp9CAMW0FVygUasZ%26response_type%3Dcode%26state%3Dnjtech%26s%3Df682b396da8eb53db80bb072f5745232"
 useragent="User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.55"
 
-lt=$(cat login_get_html.txt|grep -o -E 'LT-.{39}'|head -1|tail -c 43)
-execution=$(cat login_get_html.txt|grep -o -E 'name="execution" value=".{4}'|head -1|tail -c 5)
+lt=$(cat login_get_html.html|grep -o -E 'LT-.{39}'|head -1|tail -c 43)
+execution=$(cat login_get_html.html|grep -o -E 'name="execution" value=".{4}'|head -1|tail -c 5)
 insert_cookie=$(grep "insert_cookie" login_cookie.txt|awk '{print $7}')
 JSESSIONID=$(grep "JSESSIONID" login_cookie.txt|awk '{print $7}'|head -1)
 
@@ -31,10 +31,10 @@ form_data="username="$username"&password="$password"&channelshow="$channelshow"&
 
 # post data
 echo "[INFO] Sending data to remote host..."
-curl -kL -X POST "$posturl" -H "$useragent" -H "$cookie" -d "$form_data" -o login_post_html.txt > /dev/null 2>&1
+curl -kL -X POST "$posturl" -H "$useragent" -H "$cookie" -d "$form_data" -o login_post_html.html > /dev/null 2>&1
 
 # check wheather login succeed
-if grep -q g-ngd-top-1 login_post_html.txt ;then
+if grep -q oauth2/logout login_post_html.html ;then
   echo "[INFO] Login Succeeded!"
   echo "[DONE] $(date)" >> log.txt
 else
