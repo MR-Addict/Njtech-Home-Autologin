3b7ca417159506df993ff98a742dd6aecd82a1c1
MR-Addict <2750417853@qq.com>
2022-11-25 21:45:17 +0800

    using dddocr model

--- src/autologin.py
+++ src/autologin.py
@@ -1,71 +1,69 @@
-# 0. Import packages
 import os
 import json
 import shutil
+import ddddocr
 import requests
 from bs4 import BeautifulSoup
 
-# 1. Login preparations
-# 1.1 Load profile information
-print("[INFO] Loading your profile...")
-profile = json.load(open("./profile.json"))
-provider = {
-    "cmcc": "中国移动",
-    "telecom": "中国电信"
-}
-# 1.2 Get constants
-geturl = "https://i.njtech.edu.cn"
-posturl = "https://u.njtech.edu.cn"
-useragent = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.55'
-}
 
-captchapath = "./captcha.jpg"
-captchageturl = "https://u.njtech.edu.cn/cas/captcha.jpg"
-captchaapiurl = "http://202.119.245.12:45547"
-# 1.3 implement a Session object
-session = requests.Session()
-session.headers.update(useragent)
-# 1.5 get login page
-print("[INFO] Sending get requests...")
-res = session.get(geturl)
+class autologin():
+    def __init__(self, username, password, provider):
+        self.username = username
+        self.password = password
+        self.channel = '@'+provider
+        if provider == "cmcc":
+            self.channelshow = "中国移动"
+        elif provider == "telecom":
+            self.channelshow = "中国电信"
+        self.ocr = ocr = ddddocr.DdddOcr(
+            show_ad=False, import_onnx_path="models/captcha.onnx",
+            charsets_path="models/charsets.json")
 
-# 2. Handle response
-# 2.1 Find lt and execution payload information using BeautifulSoup
-print("[INFO] Parsing response html...")
-soup = BeautifulSoup(res.content, "html.parser")
-lt = soup.find('input', attrs={'name': 'lt'})['value']
-execution = soup.find('input', attrs={'name': 'execution'})['value']
-_eventId = soup.find('input', attrs={'name': '_eventId'})['value']
-login = soup.find('input', attrs={'name': 'login'})['value']
-posturl = posturl + soup.find('form', id='fm1').get('action')
+    def connect(self):
+        # 1. Variables
+        get_url = "https://i.njtech.edu.cn"
+        post_url = "https://u.njtech.edu.cn"
+        captcha_url = "https://u.njtech.edu.cn/cas/captcha.jpg"
+        useragent = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.55'
+        }
+        # 2 Create soup
+        session = requests.Session()
+        session.headers.update(useragent)
+        try:
+            res = session.get(get_url)
+        except:
+            "[ERROR] Cannot access network!"
+        # 3. Parse html
+        soup = BeautifulSoup(res.content, "html.parser")
+        lt = soup.find('input', attrs={'name': 'lt'})['value']
+        execution = soup.find('input', attrs={'name': 'execution'})['value']
+        _eventId = soup.find('input', attrs={'name': '_eventId'})['value']
+        login = soup.find('input', attrs={'name': 'login'})['value']
+        post_url = post_url + soup.find('form', id='fm1').get('action')
+        captcha = self.ocr.classification(session.get(captcha_url).content)
+        payload = {
+            'username': self.username,
+            'password': self.password,
+            "channel": self.channel,
+            "channelshow": self.channelshow,
+            'lt': lt, 'captcha': captcha,
+            'execution': execution,
+            '_eventId': _eventId,
+            'login': login,
+        }
+        # 4. Post and check
+        res = session.post(post_url, data=payload)
+        soup = BeautifulSoup(res.content, "html.parser")
+        session.close()
+        if soup.find("div", {"class": "user-msg-info"}) is not None:
+            return "[INFO] Autologin Succeed!"
+        else:
+            return "[INFO] Autologin Failed!"
 
-with session.get(captchageturl, stream=True) as res:
-    with open("captcha.jpg", 'wb') as f:
-        shutil.copyfileobj(res.raw, f)
-res = session.post(captchaapiurl, files={"captcha": open(captchapath, 'rb')})
-captcha = json.loads(res.text)["message"]
-os.remove(captchapath)
-# 2.2 Prepare post payload
-payload = {
-    'username': profile["username"],
-    'password': profile["password"],
-    "channelshow": provider[profile['provider']],
-    "channel": '@'+profile['provider'],
-    'lt': lt,
-    'captcha': captcha,
-    'execution': execution,
-    '_eventId': _eventId,
-    'login': login,
-}
-# 2.3 Post data to host
-print("[INFO] Sending your profile to host...")
-res = session.post(posturl, data=payload)
-session.close()
 
-# 3. Check WiFi connection
-soup = BeautifulSoup(res.content, "html.parser")
-if soup.find("div", {"class": "user-msg-info"}) is not None:
-    print("[INFO] Autologin succeed!")
-else:
-    print("[INFO] Autologin failed!")
+if __name__ == '__main__':
+    with open("profile.json", 'r') as f:
+        data = json.load(f)
+        login = autologin(data["username"], data["password"], data["provider"])
+        print(login.connect())
